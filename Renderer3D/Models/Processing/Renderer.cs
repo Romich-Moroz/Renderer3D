711c9e94816a757821f79032ea7981e76f7d84ea
Roman <romich651@gmail.com>
2021-10-31 20:31:18 +0300

    Attempt at fast rasterization

--- Renderer3D/Models/Processing/Renderer.cs
+++ Renderer3D/Models/Processing/Renderer.cs
@@ -81,6 +81,119 @@ namespace Renderer3D.Models.Processing
             }
         }
 
+        #region FastTriangleRasterization (Not working)
+
+        //private void DrawFlatTopTriangle(VertexValue v0, VertexValue v1, VertexValue v2, SceneProperties sceneProperties)
+        //{
+        //    float m0 = (v2.Coordinates.X - v0.Coordinates.X) / (v2.Coordinates.Y - v0.Coordinates.Y);
+        //    float m1 = (v2.Coordinates.X - v1.Coordinates.X) / (v2.Coordinates.Y - v1.Coordinates.Y);
+
+        //    int yStart = (int)Math.Ceiling(v0.Coordinates.Y - 0.5f);
+        //    int yEnd = (int)Math.Ceiling(v2.Coordinates.Y - 0.5f);
+
+        //    for (int y = yStart; y < yEnd; y++)
+        //    {
+        //        float px0 = m0 * (y + 0.5f - v0.Coordinates.Y) + v0.Coordinates.X;
+        //        float px1 = m1 * (y + 0.5f - v1.Coordinates.Y) + v1.Coordinates.X;
+
+        //        int xStart = (int)Math.Ceiling(px0 - 0.5f);
+        //        int xEnd = (int)Math.Ceiling(px1 - 0.5f);
+
+        //        int color = default;
+        //        if (sceneProperties.RenderProperties.RenderMode == RenderMode.Flat)
+        //        {
+        //            color = FlatShader.GetFaceColor(new TriangleValue { v0 = v0, v1 = v1, v2 = v2 }, sceneProperties.LightingProperties, sceneProperties.RenderProperties.RenderFallbackColor);
+        //        }
+
+        //        for (int x = xStart; x < xEnd; x++)
+        //        {
+        //            _bitmapWriter.DrawPixel(x, y, color);
+        //        }
+        //    }
+        //}
+
+        //private void DrawFlatBottomTriangle(VertexValue v0, VertexValue v1, VertexValue v2, SceneProperties sceneProperties)
+        //{
+        //    float m0 = (v1.Coordinates.X - v0.Coordinates.X) / (v1.Coordinates.Y - v0.Coordinates.Y);
+        //    float m1 = (v2.Coordinates.X - v0.Coordinates.X) / (v2.Coordinates.Y - v0.Coordinates.Y);
+
+        //    int yStart = (int)Math.Ceiling(v0.Coordinates.Y - 0.5f);
+        //    int yEnd = (int)Math.Ceiling(v2.Coordinates.Y - 0.5f);
+
+        //    for (int y = yStart; y < yEnd; y++)
+        //    {
+        //        float px0 = m0 * (y + 0.5f - v0.Coordinates.Y) + v0.Coordinates.X;
+        //        float px1 = m1 * (y + 0.5f - v0.Coordinates.Y) + v0.Coordinates.X;
+
+        //        int xStart = (int)Math.Ceiling(px0 - 0.5f);
+        //        int xEnd = (int)Math.Ceiling(px1 - 0.5f);
+
+        //        int color = default;
+        //        if (sceneProperties.RenderProperties.RenderMode == RenderMode.Flat)
+        //        {
+        //            color = FlatShader.GetFaceColor(new TriangleValue { v0 = v0, v1 = v1, v2 = v2 }, sceneProperties.LightingProperties, sceneProperties.RenderProperties.RenderFallbackColor);
+        //        }
+
+        //        for (int x = xStart; x < xEnd; x++)
+        //        {
+        //            _bitmapWriter.DrawPixel(x, y, color);
+        //        }
+        //    }
+        //}
+
+        //public void FastTriangleRasterization(TriangleValue t, SceneProperties sceneProperties)
+        //{
+        //    if (Calculation.IsTriangleInvisible(t))
+        //    {
+        //        return;
+        //    }
+        //    Calculation.SortTriangleVerticesByY(ref t);
+
+        //    if (t.v0.Coordinates.Y == t.v1.Coordinates.Y)
+        //    {
+        //        if (t.v1.Coordinates.X < t.v0.Coordinates.X)
+        //        {
+        //            (t.v0, t.v1) = (t.v1, t.v0);
+        //        }
+        //        DrawFlatTopTriangle(t.v0, t.v1, t.v2, sceneProperties);
+        //    }
+        //    else if (t.v1.Coordinates.Y == t.v2.Coordinates.Y)
+        //    {
+        //        if (t.v2.Coordinates.X < t.v1.Coordinates.X)
+        //        {
+        //            (t.v1, t.v2) = (t.v2, t.v1);
+        //        }
+        //        DrawFlatBottomTriangle(t.v0, t.v1, t.v2, sceneProperties);
+        //    }
+        //    else
+        //    {
+        //        float alphaSplit = (t.v1.Coordinates.Y - t.v0.Coordinates.Y) / (t.v2.Coordinates.Y - t.v0.Coordinates.Y);
+        //        Vector3 vCoords = t.v0.Coordinates + (t.v2.Coordinates - t.v0.Coordinates) * alphaSplit;
+        //        Vector3 vNormal = t.v0.Normal + (t.v2.Normal - t.v0.Normal) * alphaSplit;
+        //        Vector3 vTexture = t.v0.Texture + (t.v2.Texture - t.v0.Texture) * alphaSplit;
+
+        //        var vValue = new VertexValue
+        //        {
+        //            Coordinates = vCoords,
+        //            Normal = vNormal,
+        //            Texture = vTexture
+        //        };
+
+        //        if (t.v1.Coordinates.X < vCoords.X)
+        //        {
+        //            DrawFlatBottomTriangle(t.v0, t.v1, vValue, sceneProperties);
+        //            DrawFlatTopTriangle(t.v1, vValue, t.v2, sceneProperties);
+        //        }
+        //        else
+        //        {
+        //            DrawFlatBottomTriangle(t.v0, vValue, t.v1, sceneProperties);
+        //            DrawFlatTopTriangle(vValue, t.v1, t.v2, sceneProperties);
+        //        }
+        //    }
+        //}
+
+        #endregion
+
         public void RenderPolygon(PolygonValue polygon, SceneProperties sceneProperties)
         {
             switch (sceneProperties.RenderProperties.RenderMode)
@@ -94,6 +207,12 @@ namespace Renderer3D.Models.Processing
                     }
                     break;
                 case RenderMode.Flat:
+                    for (int i = 0; i < polygon.TriangleValues.Length; i++)
+                    {
+                        //FastTriangleRasterization(polygon.TriangleValues[i], sceneProperties);
+                        RasterizeTriangle(polygon.TriangleValues[i], sceneProperties);
+                    }
+                    break;
                 case RenderMode.Phong:
                     for (int i = 0; i < polygon.TriangleValues.Length; i++)
                     {
